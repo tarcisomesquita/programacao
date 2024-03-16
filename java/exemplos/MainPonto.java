681e1ef76c4e267a4379fe3139cf7d1d5a17c561
Tarciso Mesquita <tarcisomesquita@gmail.com>
2024-03-16 18:24:21 -0300

    Update MainPonto.java

--- java/exemplos/MainPonto.java
+++ java/exemplos/MainPonto.java
@@ -1,21 +1,21 @@
 public class MainPonto {
-	public float x,y;
-	public MainPonto(float x,float y) {
+	public float x, y;
+	public MainPonto(float x, float y) {
 		this.x = x; this.y = y;
 	}
-
-	public void move(float dx,float dy) {
-		this.x+=dx; this.y+=dy;
+	
+	public void move(float dx, float dy) {
+		this.x += dx; this.y += dy;
 	}
 
 	public String toString() {
-		return "("+this.x+","+this.y+")"; // (x,y)
+		return "(" + this.x + "," + this.y + ")"; // (x,y)
 	}
-
+	
 	public void mostra() {
-		System.out.println("("+this.x+","+this.y+")");
+		System.out.println("(" + this.x + "," + this.y + ")");
 	}
-
+	
 	public boolean igual(MainPonto outro) {
 		return ((outro.x == this.x) && (outro.y == this.y)); // this == outro?
 	}
